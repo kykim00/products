10b9936c0294ea414706487eaef7ab452620c81a
kykim00 <jeongsaeki@likelion.org>
2022-04-30 06:15:13 +0900

    feat: uesQuery로 data fetch example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
+import { useQuery } from "react-query";
+import getProductsData from "../utils/getProductsData";
+
 const MainPage = () => {
+  const { data } = useQuery("products", () => getProductsData());
+  if (!data) return null;
+  console.log(data);
   return <div>Main</div>;
 };
 
