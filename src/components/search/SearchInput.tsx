c3f7439e5b54c350f7d7bf7368c355cf08243b92
kykim00 <jeongsaeki@likelion.org>
2022-05-02 00:24:10 +0900

    feat: url 연동 검색 컴포넌트 구현

---
+++ src/components/search/SearchInput.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import useDebounce from "../../hooks/useDebounce";
+
+const SearchInput = () => {
+  const [searchedValue, setSearchedValue] = useState("");
+  const keyword = useDebounce(searchedValue);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const currentParams = Object.fromEntries(searchParams);
+    if (keyword) setSearchParams({ ...currentParams, q: keyword });
+    else setSearchParams({ ...currentParams, q: "" });
+  }, [keyword]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchedValue(e.target.value);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={handleChange}
+        value={searchedValue}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
