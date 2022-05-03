3e9fe32e140b691ebd29872e7e54b7e87cc1b100
kykim00 <jeongsaeki@likelion.org>
2022-05-03 09:41:05 +0900

    refactor: 데이터 타입 변경에 따른 코드 수정

--- client/src/components/products/List.tsx
+++ client/src/components/products/List.tsx
@@ -2,18 +2,17 @@ import { Product } from "../../types";
 import ProductGridItem from "./Item";
 
 interface ListProps {
-  list: Product[];
+  list: { products: Product[] }[];
   view: string;
 }
 
-const ProductList = ({ list, view }: ListProps) => {
-  return (
-    <>
-      {list.map((product) => (
+const ProductList = ({ list, view }: ListProps) => (
+  <>
+    {list.map((page) =>
+      page.products.map((product) => (
         <ProductGridItem {...product} key={product.club.id} view={view} />
-      ))}
-    </>
-  );
-};
-
+      ))
+    )}
+  </>
+);
 export default ProductList;
