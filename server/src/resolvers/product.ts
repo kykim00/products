7154e8da0620b82f27fde9515592cc713f114e53
kykim00 <jeongsaeki@likelion.org>
2022-05-03 09:45:24 +0900

    fix: 첫 데이터 fetch 이후 바로 모든 데이터가 fetch되던 오류 수정

--- server/src/resolvers/product.ts
+++ server/src/resolvers/product.ts
@@ -2,10 +2,8 @@ import { Resolver } from "./types";
 
 const productResolver: Resolver = {
   Query: {
-    products: (parent, { pageNum = "" }, { db }, info) => {
-      const fromIndex =
-        db.products.findIndex((product) => product.club.id === pageNum) + 1;
-      return db.products.slice(fromIndex, fromIndex + 12) || [];
+    products: (parent, { pageNum = 0 }, { db }, info) => {
+      return db.products.slice(pageNum, +pageNum + 12) || [];
     },
     product: (parent, { id }, { db }, info) => {
       const found = db.products.find((item) => item.club.id === id);
