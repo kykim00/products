d68d4435589c9f33a1256ee40ba9fe3f4713c60d
kykim00 <jeongsaeki@likelion.org>
2022-05-04 13:37:40 +0900

    feat: 필터 초기화기능 구현

--- client/src/components/filters/FilterList.tsx
+++ client/src/components/filters/FilterList.tsx
@@ -1,5 +1,7 @@
+import styled from "@emotion/styled";
 import React, { SyntheticEvent, useCallback, useState } from "react";
 import useFilterAndSearch from "../../hooks/useFilterAndSearch";
+import Button from "../common/Button";
 import CheckBoxInput from "./CheckBoxInput";
 
 const initialCheckedItems = {
@@ -33,7 +35,7 @@ const FilterList = () => {
 
   const { searchedTitle, setSearchParams } = useFilterAndSearch();
 
-  const hadleApplyFilter = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleApplyFilter = () => {
     const searchQuery = searchedTitle ? { q: searchedTitle } : null;
 
     setSearchParams({
@@ -47,9 +49,18 @@ const FilterList = () => {
     setIsVisible(false);
   };
 
-  const handleClickFilterMenu = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setIsVisible(true);
-    setClickedMenu(e.currentTarget.value);
+  const handleClickFilterMenu = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      setIsVisible(true);
+      setClickedMenu(e.currentTarget.value);
+    },
+    []
+  );
+
+  const handleResetFilter = () => {
+    setCheckedItems(initialCheckedItems);
+    setSearchParams({});
+    setIsVisible(false);
   };
 
   const handleCheckFilterItem = useCallback(
@@ -75,36 +86,78 @@ const FilterList = () => {
   );
 
   return (
-    <div>
+    <FilterContainer>
+      <ResetButton onClick={handleResetFilter}>초기화</ResetButton>
       {lists.map((list, index) => (
         <>
-          <button
+          <Button
+            variant="secondary"
             key={`${Object.keys(list)[0]}-${index}`}
             onClick={handleClickFilterMenu}
             value={Object.keys(list)[0]}
           >
             {Object.keys(list)[0]}
-          </button>
+          </Button>
         </>
       ))}
-      {isVisible &&
-        lists.map((list) => {
-          if (Object.keys(list)[0] === clickedMenu) {
-            return Object.values(list)[0].map((item: string, index: number) => (
-              <CheckBoxInput
-                key={`${item}-${index}`}
-                item={item}
-                onCheck={handleCheckFilterItem}
-                checked={Object.values(checkedItems).flat(1).includes(item)}
-              />
-            ));
-          }
-          return null;
-        })}
-
-      {isVisible && <button onClick={hadleApplyFilter}>적용</button>}
-    </div>
+      {isVisible && (
+        <>
+          <FilterContent>
+            <div>
+              {lists.map((list) => {
+                if (Object.keys(list)[0] === clickedMenu) {
+                  return Object.values(list)[0].map(
+                    (item: string, index: number) => (
+                      <li key={`${item}-${index}`}>
+                        <CheckBoxInput
+                          item={item}
+                          onCheck={handleCheckFilterItem}
+                          checked={Object.values(checkedItems)
+                            .flat(1)
+                            .includes(item)}
+                        />
+                      </li>
+                    )
+                  );
+                }
+                return null;
+              })}
+            </div>
+            <Button onClick={handleApplyFilter}>적용</Button>
+          </FilterContent>
+        </>
+      )}
+    </FilterContainer>
   );
 };
 
+const FilterContainer = styled.div`
+  position: relative;
+  margin-top: 5px;
+  button {
+    margin-right: 5px;
+  }
+  ul {
+    padding-left: 55px;
+  }
+`;
+
+const ResetButton = styled.button`
+  background-color: #fff;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+  text-decoration: underline;
+`;
+
+const FilterContent = styled.ul`
+  height: 130px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  z-index: 10;
+  button {
+    width: 30%;
+  }
+`;
 export default FilterList;
